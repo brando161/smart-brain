507c9ede8644e4d1a8a6fc539be9622a1c7ad73a
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2024-01-11 21:34:14 -0500

    Upload files

---
+++ src/components/Register/Register.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+    }
+
+    onNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    onSubmitSignIn = () => {
+        fetch("https://smart-brain-api-khsd.onrender.com/register", {
+            method: "post",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if(user.id) {
+                    this.props.loadUser(user);
+                    this.props.onRouteChange('Home');
+                }
+            })
+    }
+
+    render() {
+        return (
+            <article class="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main class="pa4 black-80">
+                    <div class="measure">
+                        <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+                            <legend class="f1 fw6 ph0 mh0">Register</legend>
+                            <div class="mt3">
+                                <label class="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
+                                <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="text" 
+                                name="name"  
+                                id="name" 
+                                onChange={this.onNameChange}/>
+                            </div>
+                            <div class="mt3">
+                                <label class="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="email" 
+                                name="email-address"  
+                                id="email-address" 
+                                onChange={this.onEmailChange}/>
+                            </div>
+                            <div class="mv3">
+                                <label class="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="password" 
+                                name="password"  
+                                id="password" 
+                                onChange={this.onPasswordChange}/>
+                            </div>
+                        </fieldset>
+                        <div class="">
+                            <input 
+                            onClick={this.onSubmitSignIn}
+                            class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Register" 
+                            />
+                        </div>
+                    </div>
+                </main>
+            </article>
+        );
+    }
+}
+
+export default Register;
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+
+    onEmailChange = (event) => {
+        this.setState({signInEmail: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({signInPassword: event.target.value})
+    }
+
+    onSubmitSignIn = () => {
+        fetch("https://smart-brain-api-khsd.onrender.com/signin", {
+            method: "post",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if(user.id) {
+                    this.props.loadUser(user);
+                    this.props.onRouteChange('Home');
+                }
+            })
+    }
+
+    render() {
+        const { onRouteChange } = this.props;
+        return (
+            <article class="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main class="pa4 black-80">
+                    <div class="measure">
+                        <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+                            <legend class="f1 fw6 ph0 mh0">Sign In</legend>
+                            <div class="mt3">
+                                <label class="db fw6 lh-copy f6" htmlForfor="email-address">Email</label>
+                                <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="email" 
+                                name="email-address"  
+                                id="email-address" 
+                                onChange={this.onEmailChange}/>
+                            </div>
+                            <div class="mv3">
+                                <label class="db fw6 lh-copy f6" htmlForfor="password">Password</label>
+                                <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="password" 
+                                name="password"  
+                                id="password" 
+                                onChange={this.onPasswordChange}/>
+                            </div>
+                        </fieldset>
+                        <div class="">
+                            <input 
+                            onClick={ this.onSubmitSignIn }
+                            class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Sign in" 
+                            />
+                        </div>
+                        <div class="lh-copy mt3">
+                            <p onClick={() => onRouteChange('Register')} class="f6 link dim black db pointer">Register</p>
+                        </div>
+                    </div>
+                </main>
+            </article>
+        )
+    }
+}
+
+export default SignIn;
